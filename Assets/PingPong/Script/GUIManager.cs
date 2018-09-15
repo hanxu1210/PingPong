afddf9eb71a47fe6623c9c100ca0f47fd6ad16ae
kingokiko <kingokiko@qq.com>
2018-09-15 20:56:15 +0800

    创建项目
    创建项目aaaaa

---
+++ Assets/PingPong/Script/GUIManager.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour {
+	[Header("StartMenu")]
+	public GameObject StartMenu;
+	public Text Level;
+	public GameObject NextButton;
+	public GameObject PreButton;
+	public Image soundImg;
+	public Sprite soundOn;
+	public Sprite soundOff;
+	public string storeLink = "your store link";
+	public string facebookLink = "your facebook link";
+	
+	[Header("UI")]
+	public GameObject UI;
+	public Text pathLeft;
+
+	public static GUIManager instance;
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		StartMenu.SetActive (true);
+		UI.SetActive (false);
+
+		if (!GlobalValue.isRestart) {
+			GlobalValue.levelPlaying = GameManager.HighLevel;
+		}
+		init ();	//init start menu
+		
+		if (GlobalValue.isSound) 
+			soundImg.sprite = soundOn;
+		else
+			soundImg.sprite = soundOff;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (GameManager.CurrentState == GameManager.GameState.Playing)
+				GameManager.instance.Restart ();
+			else
+				Application.Quit ();
+		}
+		pathLeft.text = GlobalValue.levelPlayingPathLeft + "";
+	}
+
+
+
+	//called by Button
+
+	public void Play(){
+		GameManager.instance.Play ();
+		StartMenu.SetActive (false);
+		UI.SetActive (true);
+	}
+
+	//Button Next and Pre
+	private void init(){
+		
+		Level.text = "Level " + GlobalValue.levelPlaying;
+		if (GameManager.HighLevel == 1) {
+			NextButton.SetActive (false);
+			PreButton.SetActive (false);
+		} else if (GlobalValue.levelPlaying == 1) {
+			NextButton.SetActive (true);
+			PreButton.SetActive (false);
+		} else if (GlobalValue.levelPlaying == GameManager.HighLevel) {
+			NextButton.SetActive (false);
+			PreButton.SetActive (true);
+		} else {
+			NextButton.SetActive (true);
+			PreButton.SetActive (true);
+		}
+	}
+
+	public void Next(){
+		SoundManager.PlaySfx ("Click");
+		GlobalValue.levelPlaying++;
+		init ();
+	}
+
+	public void Pre(){
+		SoundManager.PlaySfx ("Click");
+		GlobalValue.levelPlaying--;
+		init ();
+	}
+	
+	public void Sound(){
+		GlobalValue.isSound = !GlobalValue.isSound;
+		GlobalValue.isMusic = !GlobalValue.isMusic;
+		if (GlobalValue.isSound) {
+			soundImg.sprite = soundOn;
+			SoundManager.SoundVolume = 1f;
+			SoundManager.MusicVolume = 1f;
+			SoundManager.PlaySfx ("Click");
+		} else {
+			soundImg.sprite = soundOff;
+			SoundManager.SoundVolume = 0f;
+			SoundManager.MusicVolume = 0f;
+		}
+	}
+
+	public void OpenFacebook(){
+		Application.OpenURL (facebookLink);
+	}
+
+	public void OpenStoreLink(){
+		Application.OpenURL (storeLink);
+	}
+
+}
---
+++ Assets/PingPong/Script/GameManager.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * This script control almost the game: State, UI, Ads
+*/
+using UnityEngine;
+using System.Collections;
+#if UNITY_5_3
+using UnityEngine.SceneManagement;
+#endif
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+	public enum GameState{
+		Menu,
+		Playing,
+		Pause,
+		Dead
+	};
+
+	private GameState state;
+	private int score = 0;
+
+	public static GameState CurrentState{
+		get{ return instance.state; }
+		set{ instance.state = value; }
+	}
+
+	public static int Score{
+		get{ return instance.score; }
+		set{ instance.score = value; }
+	}
+
+	public static int HighLevel{
+		get{ return PlayerPrefs.GetInt(GlobalValue.levelHighest,1); }
+		set{ PlayerPrefs.SetInt(GlobalValue.levelHighest, value); }
+	}
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		state = GameState.Menu;
+
+	}
+
+	public void Play(){
+		state = GameState.Playing;
+		GlobalValue.levelPlayingPathLeft = GlobalValue.levelPlaying;	//for PlayerController
+		GlobalValue.levelPathLeft = GlobalValue.levelPlaying;		//for CreatePlatform
+
+//		AdsController.HideAds ();
+
+	}
+
+	public void GameSuccess(){
+		GlobalValue.levelPlaying++;
+
+		if (GlobalValue.levelPlaying >= HighLevel)
+			HighLevel++;		//save playerPref
+		
+		StartCoroutine (WaitForRestart (1.5f));
+	
+//		AdsController.ShowAds ();
+	
+	}
+
+	public void GameOver(){
+		state = GameState.Dead;
+		StartCoroutine (WaitForRestart (1f));
+
+//		AdsController.ShowAds ();
+	
+	}
+
+	public void Restart(){
+		StartCoroutine (WaitForRestart (0f));
+	}
+
+	IEnumerator WaitForRestart(float time){
+		yield return new WaitForSeconds (time);
+		GlobalValue.isRestart = true;
+
+		#if UNITY_5_3
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		#else
+		Application.LoadLevel (Application.loadedLevel);
+		#endif
+	}
+}
